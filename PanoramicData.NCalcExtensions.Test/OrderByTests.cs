e695044edfed00bc212c375bb12385d6c30f276b
Eugene Mayevski <emayevski@users.noreply.github.com>
2025-09-10 22:56:32 +0200

    Merge remote-tracking branch 'origin/main'

--- PanoramicData.NCalcExtensions.Test/OrderByTests.cs
+++ PanoramicData.NCalcExtensions.Test/OrderByTests.cs
@@ -9,14 +9,35 @@ public class OrderByTests : NCalcTest
 		=> new ExtendedExpression($"orderBy(list(2, 1, 3), 'n', '{expression}')")
 		.Evaluate()
 		.Should()
-		.BeEquivalentTo(expectedOrder);
+		.BeEquivalentTo(expectedOrder, options => options.WithStrictOrdering());
+
+	[Theory]
+	[InlineData("n", new[] { 1.1, 1.2, 1.3 })]
+	[InlineData("-n", new[] { 1.3, 1.2, 1.1 })]
+	public void OrderBy_SingleTermDoubles_Succeeds(string expression, double[] expectedOrder)
+		=> new ExtendedExpression($"orderBy(list(1.2, 1.1, 1.3), 'n', '{expression}')")
+		.Evaluate()
+		.Should()
+		.BeEquivalentTo(expectedOrder, options => options.WithStrictOrdering());
+
+	[Theory]
+	[InlineData("n", new[] { 0, 1.2, 1.3, 5 })]
+	[InlineData("-n", new[] { 5, 1.3, 1.2, 0 })]
+	public void OrderBy_SingleTermMixedIntsAndDoubles(string expression, double[] expectedOrder)
+		=> new ExtendedExpression($"orderBy(list(1.2, 5, 0, 1.3), 'n', '{expression}')")
+		.Evaluate()
+		.Should()
+		.BeEquivalentTo(expectedOrder, options => options.WithStrictOrdering());
 
 	[Theory]
 	[InlineData("n % 32", "n % 2", new[] { 34, 33, 1, 2 })]
-	[InlineData("n % 2", "n % 32", new[] { 33, 1, 34, 2 })]
+	[InlineData("n % 2", "n", new[] { 33, 1, 34, 2 })]
 	public void OrderBy_MultipleTerms_Succeeds(string expression1, string expression2, int[] expectedOrder)
 	=> new ExtendedExpression($"orderBy(list(34, 33, 2, 1), 'n', '{expression1}', '{expression2}')")
 	.Evaluate()
 	.Should()
 	.BeEquivalentTo(expectedOrder);
+
+	// Should use the strict order
+	//.BeEquivalentTo(expectedOrder, options => options.WithStrictOrdering());
 }
